056a601942c42411a024eb6e1176bbfea98bcb09
AAbhishek02018 <38566572+AAbhishek02018@users.noreply.github.com>
2020-10-02 13:39:49 +0530

    Update qus10.java

--- qus10.java
+++ qus10.java
@@ -4,7 +4,7 @@ import java.util.stream.Collectors;
 class FluNew {
 	public static void main(String[] args) {
 
-		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9 , 10);
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9 , 10); // we can also add array content via system.in
 		System.out.println(
 				list.stream()
 						.filter(e -> e > 5)
